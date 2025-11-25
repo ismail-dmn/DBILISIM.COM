0cbf72fd3bcfba5438d5b2cc91f5db7e563475f3
ismail duman <67826493+ismail-dmn@users.noreply.github.com>
2025-11-25 16:00:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,12 @@ const hamburger = document.getElementById('hamburger-btn');
 const navMenu = document.getElementById('nav-menu');
 const navLinks = document.querySelectorAll('.nav-link');
 
-// Menü Açma/Kapama
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     navMenu.classList.toggle('active');
     document.body.classList.toggle('menu-open');
 });
 
-// Mobilde linke tıklanınca menüyü kapat
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
         hamburger.classList.remove('active');
